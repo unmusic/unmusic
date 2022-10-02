536f536f17f7ef202fbb8d509cd1b59832b3f21f
Unmusic <zennoise.dev@gmail.com>
2022-10-02 21:06:10 +0530

    Updates

--- web/src/utils/contentful.js
+++
@@ -1,8 +0,0 @@
-const contentful = require("contentful");
-
-const contentfulClient = contentful.createClient({
-  space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
-});
-
-export default contentfulClient;
